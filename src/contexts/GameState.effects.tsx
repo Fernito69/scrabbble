d135d1a800ad6be0f951fdffc122d1cbcced726d
Fernando Calvo <fernando.calvo@agrinorm.ai>
2025-12-07 16:36:36 +0100

    Refactor game state effects

---
+++ src/contexts/GameState.effects.tsx
@@ -0,0 +1,105 @@
+import { MAX_PLAYERS } from "@/model/core.defaults";
+import { Vote, VoteType } from "@/model/core.model";
+import { useUpdateGame } from "@/services/collections/game/game.hooks";
+import { DbGamePayload } from "@/services/collections/game/game.model";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useAuth } from "./AuthContext";
+import { DefaultGame, UseGameStateEffects } from "./GameState.model";
+import { drawCards } from "@/services/collections/game/game.utils";
+
+export const useGameStateEffects = ({
+  gameId,
+  state,
+  template,
+  numPlayers,
+  isGameOrganizer,
+  initialPlayerHand,
+  initted,
+  setInitted,
+  setLocalPlayerHand,
+}: UseGameStateEffects) => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  // Mutations
+  const updateGame = useUpdateGame(gameId);
+
+  /**********/
+  // Add new players to game
+  /**********/
+  useEffect(() => {
+    if (initted || !user || !template || !state) return;
+
+    let userIsPartOfGame = state.playerIds.includes(user.uid);
+
+    if (userIsPartOfGame) return;
+
+    // If more than 4 players, redirect to lobby
+    if (numPlayers >= MAX_PLAYERS && !userIsPartOfGame) {
+      toast("We are sorry, this game is already full!");
+      navigate("/");
+      return;
+    }
+
+    // If already started, redirect to lobby
+    if (state.gameStarted && !userIsPartOfGame) {
+      toast("This game already started, you can't join anymore.");
+      navigate("/");
+      return;
+    }
+
+    // Add player to game
+    const { hand, tilePouch } = drawCards(state.tilePouch);
+    const playerIds = state.playerIds.map((v) => {
+      if (v === null && !userIsPartOfGame) {
+        userIsPartOfGame = true;
+        return user.uid;
+      }
+      return v;
+    });
+
+    const payload = {
+      tilePouch,
+      playerIds,
+      playerHands: {
+        ...state.playerHands,
+        [user.uid]: hand,
+      },
+    } as Partial<DbGamePayload>;
+
+    updateGame(payload);
+
+    setInitted(true);
+
+    return () => setInitted(false);
+  }, [state, template, user]);
+
+  /***************/
+  // Initial vote
+  /***************/
+
+  useEffect(() => {
+    if (!state || !isGameOrganizer) return;
+    if (numPlayers > 1 && !state.currentVote && !state.gameStarted) {
+      const currentVote = {
+        type: VoteType.START_VOTE,
+        description: "Waiting until all players are ready to start the game",
+        voteFinished: false,
+        votes: state.playerIds
+          .filter(Boolean)
+          .map((id) => ({ playerId: id!, voted: false })),
+      } satisfies Vote;
+
+      updateGame({ currentVote });
+    }
+  }, [state]);
+
+  /***************/
+  // Update local player hand if a change is detected in the db
+  /***************/
+  useEffect(() => {
+    setLocalPlayerHand(initialPlayerHand ?? DefaultGame.localPlayerHand);
+  }, [initialPlayerHand]);
+};
---
+++ src/contexts/GameState.model.ts
@@ -0,0 +1,40 @@
+import { DEFAULT_GAME_STATE } from "@/model/core.defaults";
+import { GameState, Move, PlayerHand } from "@/model/core.model";
+import { LanguageTemplate } from "@/services/collections/letterValueMap/languageTemplate.model";
+
+export interface GameInterface {
+  state: GameState | undefined;
+  template: LanguageTemplate | undefined;
+  initted: boolean;
+  gameId: string;
+  numPlayers: number;
+  isGameOrganizer: boolean;
+  getPlayerNumber: (playerId: string) => number;
+  localProposedMove: Move[];
+  localPlayerHand: PlayerHand;
+  setLocalProposedMove: (move: Move[]) => void;
+  setLocalPlayerHand: (hand: PlayerHand) => void;
+  isMyTurn: boolean;
+}
+
+export interface UseGameStateEffects extends GameInterface {
+  gameId: string;
+  initted: boolean;
+  setInitted: (initted: boolean) => void;
+  initialPlayerHand: PlayerHand | undefined;
+}
+
+export const DefaultGame: GameInterface = {
+  state: DEFAULT_GAME_STATE,
+  template: undefined,
+  initted: false,
+  gameId: "",
+  numPlayers: 0,
+  isGameOrganizer: false,
+  getPlayerNumber: () => 1,
+  localProposedMove: [],
+  localPlayerHand: [null, null, null, null, null, null, null],
+  setLocalProposedMove: () => {},
+  setLocalPlayerHand: () => {},
+  isMyTurn: false,
+};
