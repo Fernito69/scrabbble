227312f3b8eef34e95908f6112361f05c66d2e2a
Fernito69 <fernito@gmail.com>
2025-12-14 08:27:06 +0100

    Add proper message when no games

--- src/containers/LobbyContainer/OngoingGames/OngoingGames.tsx
+++ src/containers/LobbyContainer/OngoingGames/OngoingGames.tsx
@@ -30,76 +30,90 @@ export const OngoingGames = () => {
   return (
     <div className="flex flex-col gap-4 mt-6">
       <Separator />
-      <h2 className="text-lg font-bold">{t("lobby.yourGames")}</h2>
-      <div className="flex flex-col gap-2 rounded-lg border border-input relative min-h-[200px]">
+      <div className="flex flex-col gap-4 mt-6 relative">
         {!playerGames && !error && (
-          <OverlayWithLoader>
+          <OverlayWithLoader className="min-w-50 min-h-50">
             <i>{t("lobby.loadingGames")}</i>
           </OverlayWithLoader>
         )}
-        <Table className="text-xs">
-          <TableHeader className="bg-muted text-xs">
-            <TableRow>
-              <TableHead />
-              <TableHead>{t("lobby.createdAt")}</TableHead>
-              <TableHead>{t("lobby.players")}</TableHead>
-              <TableHead>{t("lobby.scores")}</TableHead>
-              <TableHead>{t("lobby.currentMove")}</TableHead>
-              <TableHead>{t("lobby.currentTurn")}</TableHead>
-              <TableHead className="whitespace-wrap">
-                {t("lobby.tilesLeft")}
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {playerGames
-              ?.sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-              .map((game, i) => {
-                const playerIds = game.playerIds.filter(Boolean);
-
-                return (
-                  <TableRow
-                    key={i}
-                    className="cursor-pointer"
-                    onClick={() => navigate(`/game/${game.id}`)}
-                  >
-                    <TableCell>
-                      <BoardDiorama game={game} />
-                    </TableCell>
-                    <TableCell>
-                      {new Date(game.createdAt).toLocaleString()}
-                    </TableCell>
-                    <TableCell>
-                      <div className="flex flex-row gap-2 whitespace-nowrap tracking-tight">
-                        {playerIds.map((id) => getPlayerName(id)).join(", ")}
-                      </div>
-                    </TableCell>
-                    <TableCell>
-                      {playerIds
-                        .map((id) => game.score.total[id!] ?? 0)
-                        .join(", ")}
-                    </TableCell>
-                    <TableCell>
-                      <span>
-                        {game.currentPlayerId != null ? (
-                          <PlayerBadge
-                            playerId={game.currentPlayerId}
-                            colorIndex={
-                              game.currentPlayerId === user!.uid ? 0 : 1
-                            }
-                          />
-                        ) : (
-                          "-"
-                        )}
-                      </span>
-                    </TableCell>
-                    <TableCell>{game.currentTurn || "-"}</TableCell>
-                    <TableCell>{game.tilePouch.length}</TableCell>
+        {playerGames ? (
+          <>
+            <h2 className="text-lg font-bold">{t("lobby.yourGames")}</h2>
+            <div className="flex flex-col gap-2 rounded-lg border border-input relative min-h-[200px]">
+              <Table className="text-xs">
+                <TableHeader className="bg-muted text-xs">
+                  <TableRow>
+                    <TableHead />
+                    <TableHead>{t("lobby.createdAt")}</TableHead>
+                    <TableHead>{t("lobby.players")}</TableHead>
+                    <TableHead>{t("lobby.scores")}</TableHead>
+                    <TableHead>{t("lobby.currentMove")}</TableHead>
+                    <TableHead>{t("lobby.currentTurn")}</TableHead>
+                    <TableHead className="whitespace-wrap">
+                      {t("lobby.tilesLeft")}
+                    </TableHead>
                   </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                  {playerGames
+                    ?.sort(
+                      (a, b) => b.createdAt.getTime() - a.createdAt.getTime()
+                    )
+                    .map((game, i) => {
+                      const playerIds = game.playerIds.filter(Boolean);
+
+                      return (
+                        <TableRow
+                          key={i}
+                          className="cursor-pointer"
+                          onClick={() => navigate(`/game/${game.id}`)}
+                        >
+                          <TableCell>
+                            <BoardDiorama game={game} />
+                          </TableCell>
+                          <TableCell>
+                            {new Date(game.createdAt).toLocaleString()}
+                          </TableCell>
+                          <TableCell>
+                            <div className="flex flex-row gap-2 whitespace-nowrap tracking-tight">
+                              {playerIds
+                                .map((id) => getPlayerName(id))
+                                .join(", ")}
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            {playerIds
+                              .map((id) => game.score.total[id!] ?? 0)
+                              .join(", ")}
+                          </TableCell>
+                          <TableCell>
+                            <span>
+                              {game.currentPlayerId != null ? (
+                                <PlayerBadge
+                                  playerId={game.currentPlayerId}
+                                  colorIndex={
+                                    game.currentPlayerId === user!.uid ? 0 : 1
+                                  }
+                                />
+                              ) : (
+                                "-"
+                              )}
+                            </span>
+                          </TableCell>
+                          <TableCell>{game.currentTurn || "-"}</TableCell>
+                          <TableCell>{game.tilePouch.length}</TableCell>
+                        </TableRow>
+                      );
+                    })}
+                </TableBody>
+              </Table>
+            </div>
+          </>
+        ) : (
+          <div className="text-center text-xl font-bold tracking-tight">
+            {t("lobby.noGames")}
+          </div>
+        )}
       </div>
     </div>
   );
