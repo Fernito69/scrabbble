5f43104ff8c96e2ec969df8e196ff8dbd618603e
Fernando Calvo <fernando.calvo@agrinorm.ai>
2026-01-09 19:37:00 +0100

    Attempt to fix multiple language template inits bug

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,5 @@
 import { db } from "@/config/firebase";
 import { authService } from "@/services/auth";
-import { initLanguageTemplate } from "@/services/collections/letterValueMap/languageTemplate";
 import { initUserConfig } from "@/services/collections/userConfig/userConfig";
 import { useGetUserConfig } from "@/services/collections/userConfig/userConfig.hooks";
 import { User } from "firebase/auth";
@@ -54,7 +53,6 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
   useEffect(() => {
     if (user) {
       initUserConfig(db, user);
-      initLanguageTemplate(db);
     }
   }, [user]);
 
--- src/services/collections/letterValueMap/languageTemplate.hooks.tsx
+++ src/services/collections/letterValueMap/languageTemplate.hooks.tsx
@@ -1,17 +1,27 @@
 import { db } from "@/config/firebase";
 import { useEffect, useState } from "react";
-import { getLanguageTemplatesSnapshot } from "./languageTemplate";
+import {
+  getLanguageTemplatesSnapshot,
+  initLanguageTemplate,
+} from "./languageTemplate";
 import { LanguageTemplate } from "./languageTemplate.model";
 
 export const useGetLanguageTemplates = (): LanguageTemplate[] => {
   const [languageTemplates, setLanguageTemplates] = useState<
-    LanguageTemplate[]
-  >([]);
+    LanguageTemplate[] | null
+  >(null);
 
   useEffect(
     () => getLanguageTemplatesSnapshot(db, (d) => setLanguageTemplates(d)),
     []
   );
 
-  return languageTemplates;
+  useEffect(() => {
+    // Initialize language templates if they don't exist
+    if (languageTemplates && languageTemplates.length === 0) {
+      initLanguageTemplate(db);
+    }
+  }, [languageTemplates]);
+
+  return languageTemplates ?? [];
 };
